74c2175234db4ecd1876916d2ec065d01877ab9a
kierloydbeans <schofieldkier09@gmail.com>
2026-07-22 18:13:55 +0800

    fixed the interactive map

--- dashboard-web/src/components/MapContainer.jsx
+++ dashboard-web/src/components/MapContainer.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import { MapContainer as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
-const MapContainer = ({ center = [14.65, 120.98], zoom = 13, markers = [] }) => {
+const MapContainer = ({ markers = [] }) => {
   return (
-    <LeafletMap center={center} zoom={zoom} style={{ height: '100%', width: '100%' }}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {markers.map((marker, index) => (
-        <Marker key={index} position={marker.position}>
-          <Popup>{marker.label}</Popup>
-        </Marker>
-      ))}
-    </LeafletMap>
+    // Explicit height and z-index are crucial here
+    <div style={{ height: '400px', width: '100%', position: 'relative', zIndex: 1 }}>
+      <LeafletMap 
+        center={[14.65, 120.98]} 
+        zoom={13} 
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        />
+        {markers.map((marker, idx) => (
+          <Marker key={idx} position={marker.position}>
+            <Popup>{marker.label}</Popup>
+          </Marker>
+        ))}
+      </LeafletMap>
+    </div>
   )
 }
 
